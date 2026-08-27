874c44bee0b52fa00fb382fe2f91b829554f0dbe
Pavan Varma <pavan@scrupulouslabs.io>
2026-08-28 01:08:34 +0530

    Add script for logs

--- packages-backend/db-migration/scripts/db-clear.ts
+++ packages-backend/db-migration/scripts/db-clear.ts
@@ -6,13 +6,14 @@ await Effect.gen(function* () {
   const db = yield* Db;
   yield* db.execute(sql`DROP SCHEMA public CASCADE`);
   yield* db.execute(sql`CREATE SCHEMA public`);
-}).pipe(
-  Effect.provide(Db.layerForMigrationScripts),
-  Effect.runPromiseExit
-).then(Exit.match({
-  onSuccess: identity,
-  onFailure: (cause) => {
-    console.log(Cause.pretty(cause));
-    process.exit(1);
-  }
-}))
+})
+  .pipe(Effect.provide(Db.layerForMigrationScripts), Effect.runPromiseExit)
+  .then(
+    Exit.match({
+      onSuccess: identity,
+      onFailure: (cause) => {
+        console.log(Cause.pretty(cause));
+        process.exit(1);
+      },
+    }),
+  );
